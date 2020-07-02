b5123d59b2cc31aae966bd4009b2014d633c50e8
Thien Nguyen <nguyenhoanthienit@gmail.com>
2020-07-02 11:57:16 +0700

    add project

---
+++ ExerciseProject/Model/Schedule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExerciseProject.Model
+{
+    public class Schedule
+    {
+        public int SubjectId { get; set; }
+        public int ClassId { get; set; }
+        public int DayOfWeek { get; set; }
+        public string Name { get; set; }
+        public bool Active { get; set; }
+    }
+}
---
+++ ExerciseProject/Model/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExerciseProject.Model
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DateOfBirth { get; set; }
+        public int ClassId { get; set; }
+        public bool Active { get; set; }
+    }
+}
