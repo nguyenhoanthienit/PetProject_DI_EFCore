75f8b9824b546291ca6f5b1f772a565677439ba9
Thien Nguyen <nguyenhoanthienit@gmail.com>
2020-07-03 16:19:41 +0700

    add serilog

--- ExerciseProject/Program.cs
+++ ExerciseProject/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace ExerciseProject
 {
@@ -13,11 +14,17 @@ namespace ExerciseProject
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.File("logs\\log-.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
